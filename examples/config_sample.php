c2f34d334c63713202ce9535d4321cc0ac60cfac
Mark Simonds <marksimonds@gmail.com>
2016-06-10 11:46:23 -0700

    Got rid of whatcounts_required. Moved code into config files in examples/ and tests/

--- examples/config_sample.php
+++ examples/config_sample.php
@@ -1,5 +1,5 @@
 <?php
-/**
+	/**
  * Created by PhpStorm.
  * User: Mark Simonds
  * Date: 3/15/16
@@ -8,11 +8,41 @@
  * Copy to config.php and input your realm and password for testing
  */
 
-error_reporting( E_ALL );
-ini_set( 'display_errors', 1 );
+	error_reporting( E_ALL );
+	ini_set( 'display_errors', 1 );
 
-require_once( dirname( __DIR__ ) . '/vendor/autoload.php');
-require_once( dirname( __DIR__ ) . '/src/whatcounts_required.php' );
+	require_once( dirname( __DIR__ ) . '/vendor/autoload.php');
 
-define( 'WC_REALM', '[YOUR_REALM]');
-define( 'WC_PASSWORD', '[YOUR_PASSWORD]' );
+	spl_autoload_register(function($class)
+	{
+		$class = str_replace('ZayconWhatCounts\\', '', $class);
+		$trait = str_replace('Traits', '', $class);
+
+		if (file_exists(__DIR__.'/../src/ZayconWhatCounts/'.$class.'.php'))
+		{
+			/** @noinspection PhpIncludeInspection */
+			require_once(__DIR__.'/../src/ZayconWhatCounts/'.$class.'.php');
+		}
+
+		if (file_exists(__DIR__.'/../src/ZayconWhatCounts/Models/'.$class.'.php'))
+		{
+			/** @noinspection PhpIncludeInspection */
+			require_once(__DIR__.'/../src/ZayconWhatCounts/Models/'.$class.'.php');
+		}
+
+		if (file_exists(__DIR__.'/../src/ZayconWhatCounts/Traits/'.$trait.'.php'))
+		{
+			/** @noinspection PhpIncludeInspection */
+			require_once(__DIR__.'/../src/ZayconWhatCounts/Traits/'.$trait.'.php');
+		}
+
+		if (file_exists(__DIR__.'/../tests/'.$class.'.php'))
+		{
+			/** @noinspection PhpIncludeInspection */
+			require_once(__DIR__.'/../tests/'.$class.'.php');
+		}
+	});
+
+
+	define( 'WC_REALM', '[YOUR_REALM]');
+	define( 'WC_PASSWORD', '[YOUR_PASSWORD]' );
